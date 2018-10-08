b5e7e2bc2f88e501f031e204eed1747fadf0e6c5
Đạt Lê <dle40@csc.com>
2018-10-08 18:12:32 +0700

    Update Home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
-import { Route } from '@angular/compiler/src/core';
 import { SharedModule } from './shared/shared.module';
 import { HttpClientModule } from '@angular/common/http';
 import { ServicesModule } from './services/services.module';
@@ -14,15 +13,18 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { MaterialModule } from './shared/angular-material-design/material.module';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import { Ng5SliderModule } from 'ng5-slider';
+import { HomePageComponent } from './home-page/home-page.component';
 
 const routers: Routes = [
+  { path: '', component: HomePageComponent },
   { path: 'filter', loadChildren: './filter/filter.module#FilterModule' },
   { path: 'stock-detail', loadChildren: './stock-detail/stock-detail.module#StockDetailModule' },
   { path: '**', redirectTo: 'filter' }
 ];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
